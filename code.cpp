23d0f195e261ee6eb2a3d6c894e0af1ae885afd0
Bluehplaptop <Bluehplaptop@users.noreply.github.com>
2018-02-07 11:56:04 -0400

    code.cpp

---
+++ code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include "student.h"
+using namespace std;
+
+int main;
+{
+	vector <Student>info
+	string n,c;
+	student v;
+int b;
+cout << "PLease enter your name";
+cout << "Please enter your course";
+do {
+	cin >> n >> c >> endl;
+	Student.push_back(int n, c)
+}
+while (n,c);
+cout << "My name is" << Student.n () << endl;
+cout << "My course is " << Student.c () << endl;
+v.setname  (n);
+v.setcourse(c);
+
+for (b = 0; b< 4; b++)
+{
+cout << info[b].getname();
+}
+for (b = 0; b< 4; b++)
+{
+cout << info[b].getcourse();
+}
+}
+
